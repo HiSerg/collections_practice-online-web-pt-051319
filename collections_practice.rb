a8723b8948be3f4d871e19df4b1776dae4fd91ec
Sergio Guzman <hiserg98@gmail.com>
2019-06-18 05:58:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.each do |word|
+    word.length.sort
+  end 
+end
+
+def swap_elements(arr)
+  
+end
+
+def reverse_array(arr)
+  
+end 
+
+def kesha_maker(arr)
+  
+end
+
+def find_a(arr)
+  
+end
+
+def sum_array(arr)
+  
+end
+
+def add_s(arr)
+  
+end
 
